57f16161892b1e44f45770034af08a9fdc199ab6
rod773 <104315743+rod773@users.noreply.github.com>
2024-01-09 14:19:35 +0100

    update

--- talkjs-backend/server.js
+++ talkjs-backend/server.js
@@ -1,14 +1,14 @@
-import express from 'express'
+import express from "express";
 
-import cors from 'cors'
-import bodyParser from 'body-parser';
+import cors from "cors";
+import bodyParser from "body-parser";
 
-import { LowSync, JSONFileSync } from 'lowdb'
+import { LowSync, JSONFileSync } from "lowdb";
 
-const adapter = new JSONFileSync('file.json')
-const db = new LowSync(adapter)
-db.read()
-db.data ||= { users: [] }
+const adapter = new JSONFileSync("file.json");
+const db = new LowSync(adapter);
+db.read();
+db.data ||= { users: [] };
 
 const app = express();
 const port = 3000;
@@ -17,34 +17,36 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/createUser', (req, res) => {    
-    const id = req.body.id
-    const name = req.body.name
-    const email = req.body.email
-    const photoUrl = req.body.photoUrl
-    const role = req.body.role
-    console.log(id, name, email, photoUrl, role)
-    db.data.users.push({
-        id: id,
-        name: name,
-        email: email,
-        photoUrl: photoUrl,
-        role: role,
-    })
-    db.write()
-    res.status(200).send('User created successfully')
+app.post("/createUser", (req, res) => {
+  const id = req.body.id;
+  const name = req.body.name;
+  const email = req.body.email;
+  const photoUrl = req.body.photoUrl;
+  const role = req.body.role;
+  console.log(id, name, email, photoUrl, role);
+  db.data.users.push({
+    id: id,
+    name: name,
+    email: email,
+    photoUrl: photoUrl,
+    role: role,
+  });
+  db.write();
+  res.status(200).send("User created successfully");
 });
 
-app.get('/getUser/:id', (req, res) => {    
-    const id = req.params.id
-    let record = db.data.users.find(p => p.id == id)
-    if (!record) {
-        console.log("No data found!")
-    } else {
-        console.log("== Record found ==")
-        console.log(record)
-        res.status(200).send(record)
-    }
+app.get("/getUser/:id", (req, res) => {
+  const id = req.params.id;
+  let record = db.data.users.find((p) => p.id == id);
+  if (!record) {
+    console.log("No data found!");
+  } else {
+    console.log("== Record found ==");
+    console.log(record);
+    res.status(200).send(record);
+  }
 });
 
-app.listen(port, () => console.log(`Sever up and running at port ${port}!`));
+app.listen(port, () =>
+  console.log(`Sever up and running at http://localhost:${port}`)
+);
